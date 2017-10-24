4eaf78fba59d4a4f2f2c15d74ada1d84ab1b3b1e
Roberto <roberto.arias@roche.com>
2017-10-24 18:46:57 +0200

    Added zoom slider that controls the image size

--- DependenciesVisualizer/ViewModels/DependenciesImageViewModel.cs
+++ DependenciesVisualizer/ViewModels/DependenciesImageViewModel.cs
@@ -17,13 +17,26 @@ namespace DependenciesVisualizer.ViewModels
         //[Inject]
         //public IDependenciesService Importer { private get; set; }
 
-        public byte[] MyImage
+        //public byte[] MyImage
+        //{
+        //    get
+        //    {
+        //        Image img = Image.FromFile(@"C:\Work\TFSDependenciesVisualizer\DependenciesVisualizer\Resources\green-plug.png");
+        //        return (byte[])(new ImageConverter()).ConvertTo(img, typeof(byte[]));
+        //    }
+        //}
+
+        private double zoomFactor = 1;
+        public double ZoomFactor
         {
-            get
+            get => this.zoomFactor;
+            set
             {
-                Image img = Image.FromFile(@"C:\Work\TFSDependenciesVisualizer\DependenciesVisualizer\Resources\green-plug.png");
-                return (byte[])(new ImageConverter()).ConvertTo(img, typeof(byte[]));
+                this.zoomFactor = value;
+                this.OnPropertyChanged("ZoomFactor");
             }
         }
+
+
     }
 }
